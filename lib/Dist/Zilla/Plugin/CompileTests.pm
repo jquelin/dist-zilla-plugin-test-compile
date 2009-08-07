b3cc166bca24e723588972b84bb69cf935d4506e
Jérôme Quelin <jquelin@gmail.com>
2009-08-07 10:24:55 +0200

    file::find::rule is core
    however, test::script is not, and thus skip those tests if needed

--- lib/Dist/Zilla/Plugin/CompileTests.pm
+++ lib/Dist/Zilla/Plugin/CompileTests.pm
@@ -41,27 +41,28 @@ ___[ t/00-compile.t ]___
 
 use strict;
 use warnings;
-use Test::More;
 
-if ( eval "use File::Find::Rule; 1;" and eval "use Test::Script; 1;" ) {
-    my @modules = File::Find::Rule->relative->file->name('*.pm')->in('lib');
-    my @scripts = glob "bin/*";
+use Test::More;
+use File::Find::Rule;
+
+my @modules = File::Find::Rule->relative->file->name('*.pm')->in('lib');
+my @scripts = glob "bin/*";
+
+plan tests => scalar(@modules) + scalar(@scripts);
     
-    plan tests => scalar(@modules) + scalar(@scripts);
+foreach my $file ( @modules ) {
+    my $module = $file;
+    $module =~ s/[\/\\]/::/g;
+    $module =~ s/\.pm$//;
+    is( qx{ $^X -M$module -e "print '$module ok'" }, "$module ok", "$module loaded ok" );
+}
     
-    foreach my $file ( @modules ) {
-        my $module = $file;
-        $module =~ s/[\/\\]/::/g;
-        $module =~ s/\.pm$//;
-        is( qx{ $^X -M$module -e "print '$module ok'" }, "$module ok", "$module loaded ok" );
-    }
-    
+SKIP: {
+    eval "use Test::Script; 1;";
+    skip "Test::Script needed to test script compilation", scalar(@scripts) if $@;
     foreach my $file ( @scripts ) {
         my $script = $file;
         $script =~ s!.*/!!;
         script_compiles_ok( $file, "$script script compiles" );
     }
-} else {
-    plan tests => 1;
-    ok(1); # avoid NOTESTS
 }
