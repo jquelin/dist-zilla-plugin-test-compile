25e78690a04c3c89c75d4ce096839d1cf9bc73db
fayland <fayland@gmail.com>
2009-08-07 10:07:47 +0800

    check if test module is installed

--- lib/Dist/Zilla/Plugin/CompileTests.pm
+++ lib/Dist/Zilla/Plugin/CompileTests.pm
@@ -41,25 +41,27 @@ ___[ t/00-compile.t ]___
 
 use strict;
 use warnings;
-
-use File::Find::Rule;
 use Test::More;
-use Test::Script;
 
-my @modules = File::Find::Rule->relative->file->name('*.pm')->in('lib');
-my @scripts = glob "bin/*";
-
-plan tests => scalar(@modules) + scalar(@scripts);
-
-foreach my $file ( @modules ) {
-    my $module = $file;
-    $module =~ s/[\/\\]/::/g;
-    $module =~ s/\.pm$//;
-    is( qx{ $^X -M$module -e "print '$module ok'" }, "$module ok", "$module loaded ok" );
-}
-
-foreach my $file ( @scripts ) {
-    my $script = $file;
-    $script =~ s!.*/!!;
-    script_compiles_ok( $file, "$script script compiles" );
+if ( eval "use File::Find::Rule; 1;" and eval "use Test::Script; 1;" ) {
+    my @modules = File::Find::Rule->relative->file->name('*.pm')->in('lib');
+    my @scripts = glob "bin/*";
+    
+    plan tests => scalar(@modules) + scalar(@scripts);
+    
+    foreach my $file ( @modules ) {
+        my $module = $file;
+        $module =~ s/[\/\\]/::/g;
+        $module =~ s/\.pm$//;
+        is( qx{ $^X -M$module -e "print '$module ok'" }, "$module ok", "$module loaded ok" );
+    }
+    
+    foreach my $file ( @scripts ) {
+        my $script = $file;
+        $script =~ s!.*/!!;
+        script_compiles_ok( $file, "$script script compiles" );
+    }
+} else {
+    plan tests => 1;
+    ok(1); # avoid NOTESTS
 }
