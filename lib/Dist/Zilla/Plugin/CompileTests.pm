38cadcc892708f38a519adfbf0a08e599d752c22
Jérôme Quelin <jquelin@gmail.com>
2010-01-16 14:41:53 +0100

    faking $ENV{HOME} for cpantesters
    cf http://www.mail-archive.com/cpan-testers-discuss@perl.org/msg00354.html

--- lib/Dist/Zilla/Plugin/CompileTests.pm
+++ lib/Dist/Zilla/Plugin/CompileTests.pm
@@ -114,6 +114,7 @@ use warnings;
 
 use Test::More;
 use File::Find;
+use File::Temp qw{ tempdir };
 
 my @modules;
 find(
@@ -132,16 +133,21 @@ find(
 my @scripts = glob "bin/*";
 
 plan tests => scalar(@modules) + scalar(@scripts);
-    
-is( qx{ $^X -Ilib -M$_ -e "print '$_ ok'" }, "$_ ok", "$_ loaded ok" )
-    for sort @modules;
-    
-SKIP: {
-    eval "use Test::Script; 1;";
-    skip "Test::Script needed to test script compilation", scalar(@scripts) if $@;
-    foreach my $file ( @scripts ) {
-        my $script = $file;
-        $script =~ s!.*/!!;
-        script_compiles_ok( $file, "$script script compiles" );
+
+{
+    # fake home for cpan-testers
+    local $ENV{HOME} = tempdir( CLEANUP => 1 );
+
+    is( qx{ $^X -Ilib -M$_ -e "print '$_ ok'" }, "$_ ok", "$_ loaded ok" )
+        for sort @modules;
+
+    SKIP: {
+        eval "use Test::Script; 1;";
+        skip "Test::Script needed to test script compilation", scalar(@scripts) if $@;
+        foreach my $file ( @scripts ) {
+            my $script = $file;
+            $script =~ s!.*/!!;
+            script_compiles_ok( $file, "$script script compiles" );
+        }
     }
 }
